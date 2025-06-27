75c7411a6ecf0e6db500f52dc0a05d81ecb8602c
eze-896 <irustaezequiel4@gmail.com>
2025-06-27 19:08:57 -0300

    Comentarios y Modificacion de constructor

--- Usuario.cs
+++ Usuario.cs
@@ -1,8 +1,13 @@
 public class Usuario{
+
+	// Atributos de clase
 	private int dni;
 	private string apellido;
 	
-	public Usuario(){
-	}
+	// Constructor
+	public Usuario( int user_dni, string user_apellido ){
+        dni = user_dni;
+        apellido = user_apellido;
+    }
 	
 }
